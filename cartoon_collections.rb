abb3a095d13a1f9772d3afa3b9b856e299bd85ff
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-25 00:24:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def greet_characters(array)
   array.each do |element|
-    puts "Hi, 
-    #{element}how are you doing today?"
+    puts "Hi, #{element}how are you doing today?"
 end
 
 # Use `each` to enumerate over the provided array

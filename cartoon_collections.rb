e21e97fdce300b86150cccd08b8116eb19d04722
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-25 00:26:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,10 +4,7 @@ def greet_characters(array)
 end
 end
 
-# Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
-  
+
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #

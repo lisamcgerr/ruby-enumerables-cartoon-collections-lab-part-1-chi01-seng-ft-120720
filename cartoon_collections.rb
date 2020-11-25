45b2b5c3bf6556e4e9d09d677f5b7736319eba19
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-25 00:36:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 
 def list_dwarves(array)
-  array.each_with_index do |element, index|
+  dwarves_array.each_with_index do |element, index|
     index = 0 
     num = index + 1 
     puts "#{num}. #{element}"
